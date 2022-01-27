2d94ff2bf8086d25a61ccbe11d7e8e953a1caca2
Danila_Goloshchapov <danila_goloshchapov@epam.com>
2022-01-27 21:54:57 +0300

    failover test recovered

--- src/main/scala/io/github/salamahin/stemma/tinkerpop/StemmaRepository.scala
+++ src/main/scala/io/github/salamahin/stemma/tinkerpop/StemmaRepository.scala
@@ -3,6 +3,7 @@ package io.github.salamahin.stemma.tinkerpop
 import gremlin.scala.{ScalaGraph, TraversalSource}
 import io.github.salamahin.stemma.domain._
 import org.apache.commons.lang3.exception.ExceptionUtils
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource
 
 import scala.util.Try
 
@@ -99,7 +100,7 @@ class StemmaRepository(graph: ScalaGraph, ops: StemmaOperations) {
   private def transaction[T](f: TraversalSource => Either[StemmaError, T]): Either[StemmaError, T] = {
     val tx = graph.tx()
 
-    Try(f(tx.begin()))
+    Try(f(TraversalSource(tx.begin(): GraphTraversalSource)))
       .toEither
       .leftMap(err => UnknownError(ExceptionUtils.getStackTrace(err)): StemmaError)
       .flatten
--- src/test/scala/io/github/salamahin/stemma/service/FailoverStemmaRepositoryTest.scala
+++ src/test/scala/io/github/salamahin/stemma/service/FailoverStemmaRepositoryTest.scala
@@ -6,12 +6,13 @@ import io.github.salamahin.stemma.service.graph.GRAPH
 import io.github.salamahin.stemma.service.repo.{REPO, Repository}
 import io.github.salamahin.stemma.service.stemma.STEMMA
 import io.github.salamahin.stemma.tinkerpop.{StemmaOperations, StemmaRepository}
+import zio.test.Assertion.hasSameElements
 import zio.test._
 import zio.{UIO, ZIO, ZLayer}
 
 object FailoverStemmaRepositoryTest extends DefaultRunnableSpec with Requests with RenderStemma {
-  class FailedToSetRelationOps extends StemmaOperations {
-    override def setChildRelation(ts: TraversalSource, familyId: String, personId: String): Either[StemmaError, Unit] = Left(UnknownError("failed to set child relation"))
+  class FailedToRemoveChildRelation extends StemmaOperations {
+    override def removeFamily(ts: TraversalSource, id: String): Either[NoSuchFamilyId, Unit] = Left(NoSuchFamilyId(id))
   }
 
   private val repo: ZLayer[GRAPH, Nothing, REPO] = ZIO
@@ -19,7 +20,7 @@ object FailoverStemmaRepositoryTest extends DefaultRunnableSpec with Requests wi
     .map(_.get)
     .map(graph =>
       new Repository {
-        override val repo: UIO[StemmaRepository] = graph.graph.map(g => new StemmaRepository(g, new FailedToSetRelationOps))
+        override val repo: UIO[StemmaRepository] = graph.graph.map(g => new StemmaRepository(g, new FailedToRemoveChildRelation))
       }
     )
     .toLayer
@@ -31,10 +32,11 @@ object FailoverStemmaRepositoryTest extends DefaultRunnableSpec with Requests wi
 
   private val revertChangesOnFailure = testM("any change that modifies graph would be reverted on failure") {
     for {
-      failingStemma <- service
-      _             <- failingStemma.newFamily(family(createJames, createJuly)()).catchAll(_ => ZIO.succeed())
-      stemma        <- failingStemma.stemma()
-    } yield assertTrue(stemma.people.isEmpty) && assertTrue(stemma.families.isEmpty)
+      s                                   <- service
+      Family(_, jamesId :: _ :: Nil, Nil) <- s.newFamily(family(createJames, createJuly)()).catchAll(_ => ZIO.succeed())
+      _                                   <- s.removePerson(jamesId).catchAll(_ => ZIO.succeed())
+      render(stemma)                      <- s.stemma()
+    } yield assert(stemma)(hasSameElements("(James, July) parentsOf ()" :: Nil))
   }
 
   override def spec = suite("StemmaService: corruption")(revertChangesOnFailure)
