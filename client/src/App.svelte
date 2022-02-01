4009f68e71954ba2fef55f58296b4571f6f44f3f
Danila_Goloshchapov <danila_goloshchapov@epam.com>
2022-02-02 00:56:36 +0300

    client initiated

---
+++ client/src/App.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Authenticate from "./components/Authenticate.svelte";
+	export let name: string;
+</script>
+
+<main>
+	<div class="authenticate-holder">
+		<Authenticate />
+	</div>
+</main>
+
+<style>
+	main {
+		background-image: url("/assets/bg.webp");
+		height: 100%;
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+
+	.authenticate-holder {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		height: 100%;
+	}
+</style>
---
+++ client/src/components/Authenticate.svelte
@@ -0,0 +1,25 @@
+<div class="main-container">
+    <div>
+        <h1>project stemma</h1>
+        <button>Sign in</button>
+    </div>
+</div>
+
+<style>
+    h1 {
+        font-size: 4em;
+        font-weight: 100;
+        text-align: center;
+        margin: 0 0 30px 0;
+    }
+    button {
+        width: 100%;;
+    }
+    .main-container {
+        backdrop-filter: blur(4px);
+        backdrop-filter: brightness(30%);
+        color: ghostwhite;
+        border-radius: 10px;
+        padding: 40px 60px;
+    }
+</style>
