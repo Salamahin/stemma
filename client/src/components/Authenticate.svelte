8f6d1e04f33540e20d4660c4e3c65e37d12feb29
Danila_Goloshchapov <danila_goloshchapov@epam.com>
2022-02-02 02:22:38 +0300

    landing in progress

--- client/src/components/Authenticate.svelte
+++ client/src/components/Authenticate.svelte
@@ -1,7 +1,19 @@
+<svelte:head>
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+    }
+</svelte:head>
+
 <div class="main-container">
     <div>
         <h1>project stemma</h1>
-        <button>Sign in</button>
+            <div
+                class="g-signin2"
+                id="sigin"
+                data-longtitle="true"
+                data-onsuccess="onSignIn"
+                data-width="380"
+                data-height="50"
+            />
     </div>
 </div>
 
@@ -12,14 +24,14 @@
         text-align: center;
         margin: 0 0 30px 0;
     }
-    button {
-        width: 100%;;
-    }
     .main-container {
-        backdrop-filter: blur(4px);
-        backdrop-filter: brightness(30%);
+        backdrop-filter: blur(4px)  brightness(40%);
         color: ghostwhite;
         border-radius: 10px;
         padding: 40px 60px;
+    }
+
+    #siging {
+        margin: auto;
     }
 </style>
