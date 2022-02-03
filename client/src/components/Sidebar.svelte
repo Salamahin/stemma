02d670e168e208b664b32443a9f181f9add4646d
Danila_Goloshchapov <danila_goloshchapov@epam.com>
2022-02-03 19:47:39 +0300

    auth reworked a bit

---
+++ client/src/components/Sidebar.svelte
@@ -0,0 +1,26 @@
+<script lang="ts" context="module">
+    export type Graph = {
+        name: string;
+        id: string;
+    };
+</script>
+
+<script lang="ts">
+    import Select, { Option } from "../../node_modules/@smui/select";
+
+    let fruits = ['Apple', 'Orange', 'Banana', 'Mango'];
+ 
+ let value = 'Orange';
+</script>
+
+<div class="columns margins" style="justify-content: flex-start;">
+    <div>
+        <Select bind:value label="Select Menu">
+          {#each fruits as fruit}
+            <Option value={fruit}>{fruit}</Option>
+          {/each}
+        </Select>
+     
+        <pre class="status">Selected: {value}</pre>
+      </div>
+</div>
