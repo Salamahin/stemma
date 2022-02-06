1d856d259978cbe987f4801a042ae86b1d4a9090
Danila_Goloshchapov <danila_goloshchapov@epam.com>
2022-02-06 22:45:57 +0300

    bg

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -12,11 +12,14 @@
 
 <main>
 	{#if !signedInUser}
+	<div class="authenticate-bg">
 		<div class="authenticate-holder">
 			<Authenticate
 				google_client_id="892655929422-dcdrfg3o02637q2n5h8l1j20hlvm5mib"
 			/>
 		</div>
+	</div>
+		
 	{:else}
 		<Navbar user={signedInUser} on:signOut={() => user.set(null)} />
 	{/if}
@@ -25,11 +28,15 @@
 
 <style>
 	main {
+		height: 100%;
+	}
+
+	.authenticate-bg {
 		background-image: url("/assets/bg.webp");
+		height: 100%;
 		background-position: center;
 		background-repeat: no-repeat;
 		background-size: cover;
-		height: 100%;
 	}
 
 	.authenticate-holder {
@@ -37,6 +44,6 @@
 		justify-content: center;
 		align-items: center;
 		width: 100%;
-		min-height: 100%;
+		height: 100%;
 	}
 </style>
