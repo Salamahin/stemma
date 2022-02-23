9e5cad956809c5a91a61423a06784cae19640c2a
Danila_Goloshchapov <danila_goloshchapov@epam.com>
2022-02-23 16:02:59 +0300

    d3 works?

--- client/src/components/GraphField.svelte
+++ client/src/components/GraphField.svelte
@@ -3,6 +3,11 @@
     import {Stemma} from "../model";
     import {onMount} from 'svelte';
 
+    let personR = 20
+    let familyR = 5
+    let personColor = "#6c7385"
+    let familyColor = "#3d3c3c"
+
     let stemmaS: Stemma = {
         families: [
             {
@@ -42,10 +47,13 @@
 
     let nodes = [
         ...stemmaS.people.map(p => ({
-            id: p.id
+            id: p.id,
+            name: p.name,
+            type: "person"
         })),
         ...stemmaS.families.map(f => ({
-            id: f.id
+            id: f.id,
+            type: "family"
         }))
     ]
 
@@ -53,109 +61,85 @@
         ...stemmaS.families.flatMap(f => f.children.map(c => ({
             id: `${f.id}_${c}`,
             source: f.id,
-            target: c
+            target: c,
+            type: "childOf"
         }))),
         ...stemmaS.families.flatMap(f => f.parents.map(p => ({
             id: `${p}_${f.id}`,
             source: p,
-            target: f.id
+            target: f.id,
+            type: "parentOf"
         })))
     ]
 
-    function ForceGraph({
-                            nodes, // an iterable of node objects (typically [{id}, …])
-                            links // an iterable of link objects (typically [{source, target}, …])
-                        }, {
-                            nodeId = d => d.id, // given d in nodes, returns a unique identifier (string)
-                            nodeGroup, // given d in nodes, returns an (ordinal) value for color
-                            nodeGroups, // an array of ordinal values representing the node groups
-                            nodeTitle, // given d in nodes, a title string
-                            nodeFill = "currentColor", // node stroke fill (if not using a group color encoding)
-                            nodeStroke = "#fff", // node stroke color
-                            nodeStrokeWidth = 1.5, // node stroke width, in pixels
-                            nodeStrokeOpacity = 1, // node stroke opacity
-                            nodeRadius = 5, // node radius, in pixels
-                            nodeStrength,
-                            linkSource = ({source}) => source, // given d in links, returns a node identifier string
-                            linkTarget = ({target}) => target, // given d in links, returns a node identifier string
-                            linkStroke = "#999", // link stroke color
-                            linkStrokeOpacity = 0.6, // link stroke opacity
-                            linkStrokeWidth = 1.5, // given d in links, returns a stroke width in pixels
-                            linkStrokeLinecap = "round", // link stroke linecap
-                            linkStrength,
-                            colors = d3.schemeTableau10, // an array of color strings, for the node groups
-                            width = 640, // outer width, in pixels
-                            height = 400, // outer height, in pixels
-                            invalidation // when this promise resolves, stop the simulation
-                        } = {}) {
-        // Compute values.
-        const N = d3.map(nodes, nodeId).map(intern);
-        const LS = d3.map(links, linkSource).map(intern);
-        const LT = d3.map(links, linkTarget).map(intern);
-        if (nodeTitle === undefined) nodeTitle = (_, i) => N[i];
-        const T = nodeTitle == null ? null : d3.map(nodes, nodeTitle);
-        const G = nodeGroup == null ? null : d3.map(nodes, nodeGroup).map(intern);
-        const W = typeof linkStrokeWidth !== "function" ? null : d3.map(links, linkStrokeWidth);
-        const L = typeof linkStroke !== "function" ? null : d3.map(links, linkStroke);
+    function forceGraph(nodes, links) {
 
-
-        // Replace the input nodes and links with mutable objects for the simulation.
-        nodes = d3.map(nodes, (_, i) => ({id: N[i]}));
-        links = d3.map(links, (_, i) => ({source: LS[i], target: LT[i]}));
-
-        // Compute default domains.
-        if (G && nodeGroups === undefined) nodeGroups = d3.sort(G);
-
-        // Construct the scales.
-        const color = nodeGroup == null ? null : d3.scaleOrdinal(nodeGroups, colors);
+        const width = window.innerWidth,
+            height = window.innerHeight;
 
         // Construct the forces.
-        const forceNode = d3.forceManyBody();
-        const forceLink = d3.forceLink(links).id(({index: i}) => N[i]);
-        if (nodeStrength !== undefined) forceNode.strength(nodeStrength);
-        if (linkStrength !== undefined) forceLink.strength(linkStrength);
-
         const simulation = d3.forceSimulation(nodes)
-            .force("link", forceLink)
-            .force("charge", forceNode)
+            .force("link", d3.forceLink(links).id(node => node.id).distance(() => 100).strength(1.5))
+            .force("collide", d3.forceCollide().radius(d => d.r * 20))
+            .force("repelForce", d3.forceManyBody().strength(-2500).distanceMin(85))
+            .force("charge", d3.forceManyBody())
             .force("center", d3.forceCenter())
             .on("tick", ticked);
 
-        const svg = d3.create("svg")
+        const svg = d3.select("#chart")
             .attr("width", width)
             .attr("height", height)
             .attr("viewBox", [-width / 2, -height / 2, width, height])
             .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
 
+        const defs = svg.append('defs');
+
+        defs.append('marker')
+            .attr('id', 'child-arrow')
+            .attr('viewBox', '0 0 10 6')
+            .attr('refX', 2 * familyR)
+            .attr('refY', 3)
+            .attr('markerWidth', 10)
+            .attr('markerHeight', 6)
+            .attr('markerUnits', 'userSpaceOnUse')
+            .attr('orient', 'auto')
+            .style('fill', personColor)
+            .append('path')
+            .attr('d', 'M 0 0 L 10 3 L 0 6 Z');
+
+        defs.append('marker')
+            .attr('id', 'spouse-arrow')
+            .attr('viewBox', '0 0 10 6')
+            .attr('refX', 2 * personR)
+            .attr('refY', 3)
+            .attr('markerWidth', 12)
+            .attr('markerHeight', 8)
+            .attr('markerUnits', 'userSpaceOnUse')
+            .attr('orient', 'auto')
+            .style('fill', familyColor)
+            .append('path')
+            .attr('d', 'M 0 0 L 10 3 L 0 6 Z');
+
         const link = svg.append("g")
-            .attr("stroke", typeof linkStroke !== "function" ? linkStroke : null)
-            .attr("stroke-opacity", linkStrokeOpacity)
-            .attr("stroke-width", typeof linkStrokeWidth !== "function" ? linkStrokeWidth : null)
-            .attr("stroke-linecap", linkStrokeLinecap)
+            .attr("stroke", "#c7b7b7")
+            .attr("stroke-width", 1.5)
+            .attr("stroke-linecap", "round")
+            .attr('marker-end', d => {
+                d.type == "childOf"? 'url(#child-arrow)' : 'url(#spouse-arrow)'
+            })
             .selectAll("line")
             .data(links)
             .join("line");
 
         const node = svg.append("g")
-            .attr("fill", nodeFill)
-            .attr("stroke", nodeStroke)
-            .attr("stroke-opacity", nodeStrokeOpacity)
-            .attr("stroke-width", nodeStrokeWidth)
+            .attr("fill", "red")
+            .attr("stroke", "#fff")
+            .attr("stroke-width", 5)
             .selectAll("circle")
             .data(nodes)
             .join("circle")
-            .attr("r", nodeRadius)
-            .call(drag(simulation));
-
-        if (W) link.attr("stroke-width", ({index: i}) => W[i]);
-        if (L) link.attr("stroke", ({index: i}) => L[i]);
-        if (G) node.attr("fill", ({index: i}) => color(G[i]));
-        if (T) node.append("title").text(({index: i}) => T[i]);
-        if (invalidation != null) invalidation.then(() => simulation.stop());
-
-        function intern(value) {
-            return value !== null && typeof value === "object" ? value.valueOf() : value;
-        }
+            .attr("r", node => node.type == "person" ? personR : familyR)
+            .call(drag(simulation))
 
         function ticked() {
             link
@@ -192,13 +176,10 @@
                 .on("drag", dragged)
                 .on("end", dragended);
         }
-
-        return Object.assign(svg.node(), {scales: {color}});
     }
 
     onMount(() => {
-        let svg = new ForceGraph({nodes, links})
-        document.getElementById("chart").appendChild(svg)
+        forceGraph(nodes, links)
     })
 
 
@@ -416,4 +397,11 @@
 
 </script>
 
-<div id="chart"></div>
+<svg id="chart"></svg>
+
+<style>
+    #chart {
+        width: 100%;
+        height: 100%;
+    }
+</style>
