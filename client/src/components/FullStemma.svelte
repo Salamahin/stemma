07423ae3ad51c1963cc0497e3751ba9e1f429b6c
Danila_Goloshchapov <danila_goloshchapov@epam.com>
2022-07-17 19:56:46 +0300

    adding a pin

--- client/src/components/FullStemma.svelte
+++ client/src/components/FullStemma.svelte
@@ -34,14 +34,20 @@
         mergeData(svg, nodes, relations);
 
         svg.selectAll("circle")
-            .on("mouseenter", (event, node) => {
+            .on("mouseenter", function (event, node) {
                 if (node.type == "person" && selectionController.personIsHighlighted(node.id)) {
                     selectionController.add(node.id, new GenerationSelection(stemmaIndex, node.id));
                     renderChart(svg, selectionController, stemmaIndex);
 
-                    svg.selectAll("circle")
-                        .filter((n) => n.id == node.id)
-                        .attr("r", hoveredPersonR);
+                    d3.select(this).attr("r", hoveredPersonR);
+
+                    d3.select(this.parentNode)
+                        .append("path")
+                        .attr(
+                            "d",
+                            "M4.146.146A.5.5 0 0 1 4.5 0h7a.5.5 0 0 1 .5.5c0 .68-.342 1.174-.646 1.479-.126.125-.25.224-.354.298v4.431l.078.048c.203.127.476.314.751.555C12.36 7.775 13 8.527 13 9.5a.5.5 0 0 1-.5.5h-4v4.5c0 .276-.224 1.5-.5 1.5s-.5-1.224-.5-1.5V10h-4a.5.5 0 0 1-.5-.5c0-.973.64-1.725 1.17-2.189A5.921 5.921 0 0 1 5 6.708V2.277a2.77 2.77 0 0 1-.354-.298C4.342 1.674 4 1.179 4 .5a.5.5 0 0 1 .146-.354z"
+                        )
+                        .attr("transform(translate(-20, -20))")
                 }
             })
             .on("mouseleave", (_event, node) => {
@@ -58,7 +64,7 @@
             });
 
         renderChart(svg, selectionController, stemmaIndex);
-        makeDrag(svg, simulation)
+        makeDrag(svg, simulation);
     }
 
     onMount(() => {
