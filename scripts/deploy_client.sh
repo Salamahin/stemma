0c19e37add3f3bdd7f5a6e4edf2961078f5319dc
Egor Goloshchapov <egor.goloshchapov@siemens.com>
2022-08-08 15:49:47 +0200

    aws-migration: deploy script added

--- scripts/deploy_client.sh
+++ scripts/deploy_client.sh
@@ -5,7 +5,9 @@ export html_assets_bucket="stemma-app-appbucket-1tws50417h1zw"
 export cloudfront_distribution_id="E22WOJI3A2N1Q6"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-aws s3 cp $SCRIPT_DIR/../client-demo/ s3://$html_assets_bucket/ --recursive
+aws s3 sync $SCRIPT_DIR/../client-demo/ s3://$html_assets_bucket/ \
+ --delete \
+ --size-only
 
 aws cloudfront create-invalidation \
     --distribution-id $cloudfront_distribution_id \
