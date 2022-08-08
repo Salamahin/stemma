98a3e84acd1cfd2abbaf6714cf37fe19ba107522
Egor Goloshchapov <egor.goloshchapov@siemens.com>
2022-08-08 15:43:09 +0200

    aws-migration: web assets deploy added

---
+++ scripts/deploy_client.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+set -x
+export html_assets_bucket="stemma-app-appbucket-1tws50417h1zw"
+export cloudfront_distribution_id="E22WOJI3A2N1Q6"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+aws s3 cp $SCRIPT_DIR/../client-demo/ s3://$html_assets_bucket/ --recursive
+
+aws cloudfront create-invalidation \
+    --distribution-id $cloudfront_distribution_id \
+    --paths "/*"
+
+echo "ok"
