476dc6d2aafcc82b7c77964a8cb0ddf9afe72b9c
Danila_Goloshchapov <danila_goloshchapov@epam.com>
2022-08-11 14:06:00 +0300

    base64 dec

--- api_impl_aws_lambda/src/main/scala/io/github/salamahin/stemma/apis/serverless/aws/Lambda.scala
+++ api_impl_aws_lambda/src/main/scala/io/github/salamahin/stemma/apis/serverless/aws/Lambda.scala
@@ -1,18 +1,37 @@
 package io.github.salamahin.stemma.apis.serverless.aws
 
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent
 import com.typesafe.scalalogging.LazyLogging
-import zio.json.JsonEncoder
-import zio.{Exit, Runtime, Task, Unsafe}
+import io.github.salamahin.stemma.domain.RequestDeserializationProblem
+import zio.json.{DecoderOps, EncoderOps, JsonDecoder, JsonEncoder}
+import zio.{Exit, Runtime, Task, Unsafe, ZIO}
 
-trait Lambda extends LazyLogging {
-  def runUnsafe[T: JsonEncoder](task: Task[T]) = {
-    import zio.json._
+import java.util.Base64
 
-    Unsafe.unsafe { implicit u => Runtime.default.unsafe.run(task) } match {
-      case Exit.Success(value) => value.toJson
-      case Exit.Failure(cause) =>
-        logger.error(s"Unexpected error", cause.squash)
-        throw new IllegalStateException(cause.squash)
+abstract class Lambda[In, Out](implicit jsonDecoder: JsonDecoder[In], jsonEncoder: JsonEncoder[Out]) extends LazyLogging {
+  def run(email: String, request: In): Task[Out]
+
+  final def apply(input: APIGatewayV2HTTPEvent, context: Context) = {
+    val email = ZIO.attempt(input.getRequestContext.getAuthorizer.getJwt.getClaims.get("email"))
+
+    val request = ZIO
+      .attempt(new String(Base64.getDecoder.decode(input.getBody)))
+      .flatMap(str => ZIO.fromEither(str.fromJson[In]))
+      .mapError(err => RequestDeserializationProblem(s"Failed to deser the request, details: ${err}"))
+
+    val zio = for {
+      (email, body) <- email <&> request
+      res           <- run(email, body)
+    } yield res
+
+    Unsafe.unsafe { implicit u =>
+      Runtime.default.unsafe.run(zio) match {
+        case Exit.Success(value) => value.toJson
+        case Exit.Failure(cause) =>
+          logger.error(s"Unexpected error", cause.squash)
+          throw new IllegalStateException(cause.squash)
+      }
     }
   }
 }
