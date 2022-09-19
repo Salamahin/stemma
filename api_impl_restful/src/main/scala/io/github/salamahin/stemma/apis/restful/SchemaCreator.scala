b7502c14164853f40b8a1d9f968372e0d7329c39
Danila_Goloshchapov <danila_goloshchapov@epam.com>
2022-09-19 15:08:51 +0300

    stemma-30

--- api_impl_restful/src/main/scala/io/github/salamahin/stemma/apis/restful/SchemaCreator.scala
+++ api_impl_restful/src/main/scala/io/github/salamahin/stemma/apis/restful/SchemaCreator.scala
@@ -13,15 +13,3 @@ object SchemaCreator extends ZIOAppDefault {
       .flatMap(_.createSchema)
       .provideSome(StorageService.slick)
 }
-
-object A extends App {
-  val ds = new PGSimpleDataSource();
-  ds.setUrl("jdbc:postgresql://free-tier13.aws-eu-central-1.cockroachlabs.cloud:26257/defaultdb?options=--cluster%3Dproud-gnoll-3170&sslmode=verify-full&sslrootcert=s3://stemma-app-junk-123/root.crt");
-  ds.setUser("stemma-admin")
-  ds.setPassword("VRB9irySLqaA4ncVE4baGQ")
-
-  var conn: Connection = null;
-  conn = ds.getConnection();
-  val stat = conn.createStatement();
-  val rs   = stat.executeQuery("SELECT 1");
-}
