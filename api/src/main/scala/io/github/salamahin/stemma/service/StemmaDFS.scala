243a27b492ed4db64b1dea2aa4d3d599c8132823
Danila_Goloshchapov <danila_goloshchapov@epam.com>
2022-09-23 01:27:53 +0300

    dfs refactored a bit

--- api/src/main/scala/io/github/salamahin/stemma/service/StemmaDFS.scala
+++ api/src/main/scala/io/github/salamahin/stemma/service/StemmaDFS.scala
@@ -5,10 +5,10 @@ import io.github.salamahin.stemma.domain.Stemma
 import scala.collection.mutable
 
 class StemmaDFS(stemma: Stemma) {
-  def hasCycles(): Boolean = {
-    def familyId(id: String) = s"family_$id"
-    def personId(id: String) = s"person_$id"
+  private def familyId(id: String) = s"family_$id"
+  private def personId(id: String) = s"person_$id"
 
+  def hasCycles(): Boolean = {
     val adjacency = stemma
       .families
       .flatMap { fd =>
@@ -29,24 +29,25 @@ class StemmaDFS(stemma: Stemma) {
       .toMap
 
     val visited = mutable.Set.empty[String]
-    val parent  = mutable.Map.empty[String, String]
+    val parents  = mutable.Map.empty[String, String]
 
-    def isCyclic(v: String, par: Option[String]): Boolean = {
-      visited += v
-      for (u <- adjacency(v)) {
-        if (!par.contains(u)) {
-          if (visited(u)) return true
+    def isCyclic(node: String, nodeParent: Option[String]): Boolean = {
+      visited += node
 
-          parent(u) = v
-
-          if (isCyclic(u, Some(v))) return true
-        }
+      for {
+        u <- adjacency(node)
+        if !nodeParent.contains(u)
+      } {
+        if (visited(u)) return true
+        parents(u) = node
+        if (isCyclic(u, Some(node))) return true
       }
+
       false
     }
 
     for (v <- adjacency.keys) {
-      if (!visited(v) && isCyclic(v, parent.get(v))) return true
+      if (!visited(v) && isCyclic(v, parents.get(v))) return true
     }
 
     false
