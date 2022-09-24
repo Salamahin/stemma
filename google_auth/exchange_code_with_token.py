3717a20244b236436a60815eab2206a1cfa222b1
Egor Goloshchapov <egor.goloshchapov@siemens-energy.com>
2022-09-24 22:46:22 +0200

    working code flow implemented via lambdas

---
+++ google_auth/exchange_code_with_token.py
@@ -0,0 +1,50 @@
+import json
+from datetime import date
+
+import google_auth_oauthlib
+import pkg_resources
+
+
+def lambda_handler(event, context):
+    query_str: str = event["rawQueryString"]
+    domain_name: str = event["requestContext"]["domainName"]
+    https_domain_name = f"https://{domain_name}"
+    credentials = exchange_code_to_token(https_domain_name, query_str)
+    expires: date = credentials.expiry
+    response = {
+        "statusCode": 302,
+        "body": json.dumps({}),
+        "headers": {'Location': f"https://stemma.link"},
+        "cookies": [
+            cookie("token", credentials.token, expires),
+            cookie("refresh_token", credentials.refresh_token, expires)
+        ],
+    }
+    return response
+
+
+def cookie(key: str, value: str, expiration: date) -> str:
+    exp = expiration.strftime("%a, %d %b %Y %H:%M:%S GMT")
+    return f"{key}={value}; expires={exp}; Path=/; Secure=true; Domain=.stemma.link"
+
+
+def exchange_code_to_token(https_domain_name: str, query_params: str) -> str:
+    flow = create_flow()
+    flow.redirect_uri = f"{https_domain_name}/oauth2/idpresponse"
+    flow.fetch_token(authorization_response=f"{https_domain_name}?{query_params}")
+    return flow.credentials
+
+
+def create_flow():
+    secret_path = pkg_resources.resource_filename(__name__, "secret.json")
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        secret_path,
+        scopes=['https://www.googleapis.com/auth/userinfo.email',
+                'https://www.googleapis.com/auth/userinfo.profile',
+                'openid'])
+    return flow
+
+
+if __name__ == '__main__':
+    exchange_code_to_token(
+        "state=vZUKlFKMyhRaxVupgtV4pRlhwOtGbd&code=4%2F0ARtbsJqSeDqM2huOPE9yFYZ9D12YghGZdf3cin5wd4U66a2jJInDmUMxpkICTP9j-7DS_A&scope=email+profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+openid&authuser=0&prompt=consent")
---
+++ google_auth/initiate_login.py
@@ -0,0 +1,38 @@
+import json
+
+import google_auth_oauthlib
+import pkg_resources
+
+
+def lambda_handler(event, context):
+    authorization_url, state = generate_login_url()
+    response = {"statusCode": 302,
+                "headers": {'Location': authorization_url},
+                "body": json.dumps({})
+                }
+    return response
+
+
+def generate_login_url():
+    flow = create_flow()
+    flow.redirect_uri = 'https://api.stemma.link/oauth2/idpresponse'
+    authorization_url, state = flow.authorization_url(
+        access_type='offline',
+        include_granted_scopes='true',
+        approval_prompt='force')
+    return authorization_url, state
+
+
+def create_flow():
+    secret_path = pkg_resources.resource_filename(__name__, "secret.json")
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        secret_path,
+        scopes=['https://www.googleapis.com/auth/userinfo.email',
+                'https://www.googleapis.com/auth/userinfo.profile',
+                'openid'])
+    return flow
+
+
+if __name__ == '__main__':
+    url = generate_login_url()
+    print(url)
